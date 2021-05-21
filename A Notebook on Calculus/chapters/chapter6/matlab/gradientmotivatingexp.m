4524a94bb01e0d10662b0aa95df14f9bf5e2ab59
sunluelectric <sunlu.electric@gmail.com>
2021-05-21 16:42:48 +0800

    update chapter6

--- A Notebook on Calculus/chapters/chapter6/matlab/gradientmotivatingexp.m
+++ A Notebook on Calculus/chapters/chapter6/matlab/gradientmotivatingexp.m
@@ -10,7 +10,7 @@ surf(X1,X2,Y)
 plot3(1,0,2*sin(1)+cos(0.5+pi/2)+sin(0)+3,'r.','markersize',20)
 xlabel('$x_1$', 'interpreter', 'latex')
 ylabel('$x_2$', 'interpreter', 'latex')
-zlabel('$y = f(x_1,x_2)$', 'interpreter', 'latex')
+zlabel('$y = f(x)$', 'interpreter', 'latex')
 axis([-3,3,-2,2,0,8])
 grid on
 view(-40,30)
@@ -43,7 +43,7 @@ plot3([1,1],[0,1],[2*sin(1) + sin(0.5*1+pi)+sin(2*0)+3,2*sin(1) + sin(0.5*1+pi)+
 plot3(1,0,2*sin(1)+cos(0.5+pi/2)+sin(0)+3,'r.','markersize',20)
 xlabel('$x_1$', 'interpreter', 'latex')
 ylabel('$x_2$', 'interpreter', 'latex')
-zlabel('$y = f(x_1,x_2)$', 'interpreter', 'latex')
+zlabel('$y = f(x)$', 'interpreter', 'latex')
 axis([-3,3,-2,2,0,8])
 grid on
 view(-40,30)
@@ -68,12 +68,64 @@ plot3([1,1],[0,1],[2*sin(1) + sin(0.5*1+pi)+sin(2*0)+3,2*sin(1) + sin(0.5*1+pi)+
 plot3(1,0,2*sin(1)+cos(0.5+pi/2)+sin(0)+3,'r.','markersize',20)
 xlabel('$x_1$', 'interpreter', 'latex')
 ylabel('$x_2$', 'interpreter', 'latex')
-zlabel('$y = f(x_1,x_2)$', 'interpreter', 'latex')
+zlabel('$y = f(x)$', 'interpreter', 'latex')
 axis([-3,3,-2,2,0,8])
 grid on
 view(-40,30)
 
+%% Progress
+tmax = 100;
+alpha_rate = 0.05;
+x_trajectory = zeros(2,tmax);
+y_trajectory = zeros(1,tmax);
+x_trajectory_init = [1;0];
+y_trajectory_init = fx(x_trajectory_init);
+for k = 0:tmax
+    if k==0
+        grad = gradfx(x_trajectory_init);
+%         disp(grad)
+        x_trajectory(:,k+1) = x_trajectory_init + alpha_rate*grad;
+        y_trajectory(k+1) = fx(x_trajectory(:,k+1));
+    else
+        grad = gradfx(x_trajectory(:,k));
+        x_trajectory(:,k+1) = x_trajectory(:,k) + alpha_rate*grad;
+        y_trajectory(k+1) = fx(x_trajectory(:,k+1));
+        if abs(y_trajectory(k+1) - y_trajectory(k)) < 0.001
+            tmax = k+1;
+            break
+        end
+    end
+end
 
+figure
+hold on
+surf(X1,X2,Y,'edgecolor','none')
+alpha 0.3
+plot3(1,0,2*sin(1)+cos(0.5+pi/2)+sin(0)+3,'r.','markersize',20)
+plot3([x_trajectory_init(1),x_trajectory(1,1:tmax)],...
+    [x_trajectory_init(2),x_trajectory(2,1:tmax)],...
+    [y_trajectory_init,y_trajectory(1:tmax)],'r--','linewidth',1)
+xlabel('$x_1$', 'interpreter', 'latex')
+ylabel('$x_2$', 'interpreter', 'latex')
+zlabel('$y = f(x)$', 'interpreter', 'latex')
+axis([-3,3,-2,2,0,8])
+grid on
+view(-40,30)
 
+figure
+hold on
+plot(1,0,'r.','markersize',20)
+contour(X1,X2,Y,[0.7:0.5:5.2,5.3],'showtext','on')
+plot([x_trajectory_init(1),x_trajectory(1,1:tmax)],[x_trajectory_init(2),x_trajectory(2,1:tmax)],'r--','linewidth',1)
+xlabel('$x_1$', 'interpreter', 'latex')
+ylabel('$x_2$', 'interpreter', 'latex')
+grid on
+
+function y = fx(x)
+    y = 2*sin(x(1)) + sin(x(1)/2+pi) + sin(2*x(2))+3;
+end
+function y = gradfx(x)
+    y = [2*cos(x(1))+1/2*cos(x(1)/2+pi); 2*cos(2*x(2))];
+end
 
 
